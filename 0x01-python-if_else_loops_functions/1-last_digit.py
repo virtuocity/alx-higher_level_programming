1d1536e0a51b412f91404b4d7c6c874bc6b554d0
gabaya <gabayaconsult@gmail.com>
2022-11-30 18:21:38 +0000

    Last digit pycode alter

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,12 +4,17 @@ number = random.randint(-10000, 10000)
 str1 = "and is greater than 5"
 str2 = "and is less than 6 and not 0"
 lastDigit = abs(number) % 10
-if lastDigit > 5:
-    print(f"Last digit of {number} is {lastDigit} {str1}")
-elif lastDigit == 0:
-    print(f"Last digit of {number} is {lastDigit} and is 0")
-elif lastDigit != 0 and lastDigit < 6:
-    print(f"Last digit of {number} is {lastDigit} {str2}")
+if number > 0:
+    if lastDigit > 5:
+        print(f"Last digit of {number} is {lastDigit} {str1}")
+    elif lastDigit == 0:
+        print(f"Last digit of {number} is {lastDigit} and is 0")
+    elif lastDigit != 0 and lastDigit < 6:
+        print(f"Last digit of {number} is {lastDigit} {str2}")
 if number < 0:
-    if lastDigit != 0 and lastDigit < 6:
-        print(print(f"Last digit of {number} is {lastDigit * -1} {str2}"))
+    if lastDigit > 5:
+        print(f"Last digit of {number} is {lastDigit * -1} {str2}")
+    elif lastDigit != 0 and lastDigit < 6:
+        print(f"Last digit of {number} is {lastDigit * -1} {str2}")
+    elif lastDigit == 0:
+        print(f"Laast digit of {number} is {lastDigit} and is 0")
