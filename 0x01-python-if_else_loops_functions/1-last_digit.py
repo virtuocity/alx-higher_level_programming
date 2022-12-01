67beed9d0d0d8d84d12fa5951f210abe98863611
gabaya <gabayaconsult@gmail.com>
2022-12-01 09:17:08 +0000

    Task 1 correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-str1 = "and is greater than 5"
-str2 = "and is less than 6 and not 0"
-lastDigit = abs(number) % 10
-if number > 0:
-    if lastDigit > 5:
-        print(f"Last digit of {number} is {lastDigit} {str1}")
-    elif lastDigit == 0:
-        print(f"Last digit of {number} is {lastDigit} and is 0")
-    elif lastDigit != 0 and lastDigit < 6:
-        print(f"Last digit of {number} is {lastDigit} {str2}")
+last_digit = abs(number) % 10
+string = "and is less than 6 and not 0"
+string2 = "and is greater than 5"
+if number >= 0:
+    if last_digit > 5:
+        print("Last digit of", number, "is", last_digit, string2)
+    elif last_digit == 0:
+        print("Last digit of", number, "is", last_digit, "and is 0")
+    else:
+        print("Last digit of", number, "is", last_digit, string)
+
 if number < 0:
-    if lastDigit > 5:
-        print(f"Last digit of {number} is {lastDigit * -1} {str2}")
-    elif lastDigit != 0 and lastDigit < 6:
-        print(f"Last digit of {number} is {lastDigit * -1} {str2}")
-    elif lastDigit == 0:
-        print(f"Laast digit of {number} is {lastDigit} and is 0")
+    if last_digit != 0:
+        print("Last digit of", number, "is", last_digit * -1, string)
+    else:
+        print("Last digit of", number, "is", last_digit, "and is 0")
