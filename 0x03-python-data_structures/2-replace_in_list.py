6b17d38d98bec5e6d0198e6fc858c285750cabe0
gabaya <gabayaconsult@gmail.com>
2022-12-07 08:23:27 +0000

    Task 2 pycodestyle correction

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx,element):
+def replace_in_list(my_list, idx, element):
     sizeoflist = len(my_list)
     if idx > sizeoflist - 1 or idx < 0 or my_list is None or idx is None:
         return (my_list)
