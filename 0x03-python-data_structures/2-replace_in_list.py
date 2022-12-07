18ca4465835717ae172cf0a4e99bbbcb9764ce03
gabaya <gabayaconsult@gmail.com>
2022-12-07 08:19:16 +0000

    Task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx,element):
+    sizeoflist = len(my_list)
+    if idx > sizeoflist - 1 or idx < 0 or my_list is None or idx is None:
+        return (my_list)
+    my_list[idx] = element
+    return (my_list)
