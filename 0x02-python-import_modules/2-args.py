52634897dbf77ec0afe9334fb9136bf6c7b57fc6
Buno Woche <bunogache@gmail.com>
2024-01-04 18:25:09 +0300

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == "__main":
     import sys
-    args = sys.argv
-    num_of_args = len(sys.argv)
-    if num_of_args == 1:
-	    print("{} arguments.".format(numofargs - 1))
-    elif num_of_args == 2:
-	    print("{} argument.".format(numofargs - 1))
-		print("{}: {}".format(1, args[1]))
-	else:
-		print("{} arguments.".format(numofargs - 1))
-		for i in range(1, num_of_args):
-		    print("{}: {}".format(i, args[i]))
+    args_list = sys.argv
+    len_args = len(sys.argv)
+    if len_args == 1:
+        print("{} arguments.".format(len_args - 1))
+    elif len_args == 2:
+        print("{} argument.".format(len_args - 1))
+        print("{}: {}".format(1, args_list[1]))
+    else:
+        print("{} arguments.".format(len_args - 1))
+        for i in range(1, len_args):
+            print("{}: {}".format(i, args_list[i]))
