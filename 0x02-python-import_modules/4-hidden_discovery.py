0293a94696bbb4d4794e353d725c2ebc1f84c203
gabaya <gabayaconsult@gmail.com>
2024-01-04 10:42:28 +0000

    Hotfix: prevent code from running when imported

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 hidden_names = dir(hidden_4)
-for i in hidden_names:
-    if i[:2] != '__':
-        print("{}".format(i))
+if __name__ == "__main__":
+    for i in hidden_names:
+        if i[:2] != '__':
+            print("{}".format(i))
