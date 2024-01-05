918c9ae9eb237b52e89625077638a45d3ed0ab9a
Buno Woche <bunogache@gmail.com>
2024-01-05 14:22:10 +0300

    indent correction

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-        sizeoflist = len(my_list)
-        if idx < 0:
-            return None
-        if idx > sizeoflist - 1 or idx is None or idx < 0 or my_list is None:
-            return my_list
-        my_list[idx] = element
-        return (my_list)
+    sizeoflist = len(my_list)
+    if idx < 0:
+        return None
+    if idx > sizeoflist - 1 or idx is None or idx < 0 or my_list is None:
+        return my_list
+    my_list[idx] = element
+    return (my_list)
