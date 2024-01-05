1c98b47ecd01152d9507e09a62544146414da8ee
Buno Woche <bunogache@gmail.com>
2024-01-05 20:02:51 +0300

    max int

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list.copy()
+    new_list = my_list[:]
     sizeoflist = len(my_list)
     if idx > sizeoflist - 1 or idx is None or idx < 0 or my_list is None:
         return None
