bced48d1f78c95b3358006cdcf05d01ac24b773e
Buno Woche <bunogache@gmail.com>
2024-01-06 11:30:29 +0300

    Hotfix: return none if idx < 0

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     sizeoflist = len(my_list)
-    if idx > sizeoflist - 1 or idx is None or idx < 0 or not my_list:
+    if idx < 0:
+        return None
+    if idx > sizeoflist - 1 or idx is None  or not my_list:
         return (my_list)
     my_list[idx] = element
     return (my_list)
