708f18d55e32bf2346fb6fb98c1c0d2c2ebf33a3
Buno Woche <bunogache@gmail.com>
2024-01-06 11:27:51 +0300

    Check for empty lisy

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     sizeoflist = len(my_list)
-    if idx > sizeoflist - 1 or idx is None or idx < 0:
+    if idx > sizeoflist - 1 or idx is None or idx < 0 of not my_list:
         return (my_list)
     my_list[idx] = element
     return (my_list)
