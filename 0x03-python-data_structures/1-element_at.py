677cfee580688c3870c74478c6bad5fcef41ab30
Buno Woche <bunogache@gmail.com>
2024-01-06 11:33:25 +0300

    Hotfix: return none if idx < 0II

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     sizeoflist = len(my_list)
     if idx < 0:
         return None
-    if idx > sizeoflist - 1 or idx is None or idx < 0 or my_list is None:
-        return []
+    if idx > sizeoflist - 1 or idx is None or my_list is None:
+        return None
     return (my_list[idx])
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     sizeoflist = len(my_list)
     if idx < 0:
         return None
-    if idx > sizeoflist - 1 or idx is None  or not my_list:
+    if idx > sizeoflist - 1 or idx is None or not my_list:
         return (my_list)
     my_list[idx] = element
     return (my_list)
