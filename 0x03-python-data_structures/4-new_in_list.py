fbba69484be824a81e74ca27563a6c9f0805d1a2
Buno Woche <bunogache@gmail.com>
2024-01-08 11:19:37 +0300

    new in listII

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list[:]
+    new_list = my_list.copy()
     sizeoflist = len(my_list)
     if idx > sizeoflist - 1 or idx is None or idx < 0 or my_list is None:
         return None
