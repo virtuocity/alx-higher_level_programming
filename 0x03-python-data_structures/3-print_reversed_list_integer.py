27f3b9e28f58249c2dd045b45ffa1761632dbf0b
Buno Woche <bunogache@gmail.com>
2024-01-08 11:10:54 +0300

    check if is list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if not my_list:
-        print("{}".format(my_list))
-    len_list = len(my_list)
-    my_list.reverse()
-    for i in range(0, len_list):
-        print("{:d}".format(my_list[i]))
+    if isinstance(my_list, list):
+        my_list.reverse()
+    for i in my_list:
+        print("{:d}".format(i))
