bab6ddbc2c43e8264b005961ecb600aa0d308dcd
Buno Woche <bunogache@gmail.com>
2024-01-08 17:49:24 +0300

    Delete at indexII

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,8 +3,5 @@ def delete_at(my_list=[], idx=0):
     len_list = len(my_list)
     if idx > len_list - 1 or idx is None or not my_list:
         return my_list
-    new_list = []
-    for i in range(0, len_list):
-        if i != idx:
-            new_list.append(my_list[i])
-    return (new_list)
+    del my_list[idx]
+    return (my_list)
