986da3bd75d74d6fe8b9c2990648f17a26662eab
Buno Woche <bunogache@gmail.com>
2024-01-08 17:45:54 +0300

    Divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     if len(my_list) == 0:
-        return my_list  
+        return None
     for i in range(0, len(my_list)):
         if my_list[i] % 2 == 0:
             new_list.append(True)
