e2e92f5ff0cb072b7b4b22dea77fd64d5778af24
Buno Woche <bunogache@gmail.com>
2024-01-24 17:04:29 +0300

    square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+square class
+'''
+
+
+class Square:
+    '''
+    class square with private instance attribute size
+    '''
+    def __init__(self, size):
+        self.__size = size
