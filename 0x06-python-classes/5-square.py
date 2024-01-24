27880c05749c5f356000f26a6ca2c2400dc966e2
Buno Woche <bunogache@gmail.com>
2024-01-24 17:33:25 +0300

    square class 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''
+square class
+'''
+
+
+class Square:
+    '''
+    class square with private instance attribute size
+    '''
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for j in range(self.__size):
+            for i in range(self.__size):
+                print("{}".format("#"), end='')
+            print()
