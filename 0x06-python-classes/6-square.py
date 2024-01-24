97dc9d3c3d11eb80f2cb0ab5b2dee49fe4869d52
Buno Woche <bunogache@gmail.com>
2024-01-24 17:48:51 +0300

    square class 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+'''
+square class
+'''
+
+
+class Square:
+    '''
+    class square with private instance attribute size
+    '''
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if type(position) is not tuple or len(position) != 2 or\
+           type(position[0]) != int or type(position[1]) != int or\
+           position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """ return the value of position"""
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        if type(position) is not tuple or len(position) != 2 or\
+           type(position[0]) != int or type(position[1]) != int or\
+           position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for j in range(self.__size):
+            for i in range(self.__size):
+                print("{}".format("#"), end='')
+            print()
