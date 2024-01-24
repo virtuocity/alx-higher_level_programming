4550f2371d38342b92c5f6d4ce00001bfbe0f12e
Buno Woche <bunogache@gmail.com>
2024-01-24 17:08:44 +0300

    square class 3

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+square class
+'''
+
+
+class Square:
+    '''
+    class square with private instance attribute size
+    '''
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
