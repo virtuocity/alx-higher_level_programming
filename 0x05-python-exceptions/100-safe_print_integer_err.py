127ece861e5fba539d1b911a7605004844f857a8
Buno Woche <bunogache@gmail.com>
2024-01-25 10:29:57 +0300

    Exception as eII

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,14 +2,15 @@
 """ a function that prints an integer """
 import sys
 
+
 def safe_print_integer_err(value):
     """ safe_print_integer_err - prints integer
-        Args:
-            value: can be any type (integer, string, 
-            etc.)
-        Return:
-            True if value has been correctly printed 
-            (it means the value is an integer)
+    Args:
+        value: can be any type (integer, string,
+        etc.)
+    Return:
+        True if value has been correctly printed
+        (it means the value is an integer)
     """
     try:
         print("{:d}".format(value))
