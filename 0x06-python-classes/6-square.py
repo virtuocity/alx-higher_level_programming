4cbe2429588e0309379628de1cefb65dc196ede8
gabaya <gabayaconsult@gmail.com>
2024-01-28 13:51:11 +0000

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,12 +51,9 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        else:
-            for j in range(self.__position[1]):
-                print()
-            for i in range(self.__size):
-                for j in range(self.__position[0]):
-                        print("{}". format(" "), end="")
-                for j in range(self.__size):
-                        print("{}". format("#"), end="")
-                print()
+		for i in range(self.__size):
+			for j in range(self.__position[0]):
+				print("{}".format(" ", end=""))
+			for j in range(self.__size):
+				print("{}". format("#"), end="")
+			print()
