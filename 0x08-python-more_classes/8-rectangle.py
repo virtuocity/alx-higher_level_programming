bb33eaf1e738ca47dda1cc955158aeb437256b7a
Buno Woche <bunogache@gmail.com>
2024-01-29 20:21:43 +0300

    bigger rectangleIII

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -74,11 +74,11 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if type(rect_1) != Rectangle:
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if type(rect_2) != Rectangle:
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if rect_1.area() > rect_2.area():
+        if rect_1.area() >= rect_2.area():
             return rect_1
         else:
             return rect_2
