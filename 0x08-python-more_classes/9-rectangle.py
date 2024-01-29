94bf2733c4882075154d3b092c3e59f23c655801
Buno Woche <bunogache@gmail.com>
2024-01-29 20:20:34 +0300

    bigger rectangleII

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -78,7 +78,12 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) != Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if rect_1.area() > rect_2.area():
+        if rect_1.area() >= rect_2.area():
             return rect_1
         else:
             return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        square_one = cls(size, size)
+        return square_one
