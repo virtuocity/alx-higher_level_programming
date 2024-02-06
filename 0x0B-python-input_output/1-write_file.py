6b4b39bc56460e7f71ff40d3d6bbda4c65ef2457
Buno Woche <bunogache@gmail.com>
2024-02-06 12:50:51 +0300

    Write to file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Write text to a file"""
+
+
+def write_file(filename="", text=""):
+    """
+    write_file - write  text to file
+    Args:
+        @filename: file to be opened for writing
+        @text: text to be input to file
+    Return : number of characters writtent to file i.e
+        the return value of the write() function
+    """
+    # check if file or text args is absent/empty?
+    if filename is None or text is None:
+        return
+    n = 0
+    with open(filename, "w", encoding="utf-8") as f:
+        n = f.write(text)
+    return (n)
