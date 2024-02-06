fb2d234ef5bb7a278604d7a4909a42be2ca541d8
Buno Woche <bunogache@gmail.com>
2024-02-06 16:01:00 +0300

    file_append PEP8

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """ Append to a file"""
 
+
 def append_write(filename="", text=""):
-	"""
-	append_write - append data/text to file
-	Args: 
-		@filename: file to be appended
-		@text: text to append
-	Return:
-		number of characters appended
-	"""
-	n = 0
-	with open(filename, "a", encoding="utf-8") as f:
-		n = f.write(text)
-	return (n)
+    """
+    write_file - write  text to file
+    Args:
+        @filename: file to be opened for writing
+        @text: text to be input to file
+    Return : number of characters writtent to file i.e
+        the return value of the write() function
+    """
+    # check if file or text args is absent/empty?
+    n = 0
+    with open(filename, "a", encoding="utf-8") as f:
+        n = f.write(text)
+    return (n)
