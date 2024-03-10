d8150ee53485ec055e903a504c1e80dbb38c40fc
gabaya <gabayaconsult@gmail.com>
2024-03-10 18:03:59 +0000

    ff changeII

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 """class mylist"""
 
@@ -13,20 +12,4 @@ class MyList(list):
         Sorted list
         """
         print(sorted(self))
-=======
-#!/usr/bin/python3
-"""class mylist"""
 
-
-class MyList(list):
-    """Implement lists"""
-
-    def print_sorted(self):
-        """Sort a list
-        Args:
-        list object
-        Return:
-        Sorted list
-        """
-        print(sorted(self))
->>>>>>> decfc5a425887e43df746cdede6b30d405bbb65c
