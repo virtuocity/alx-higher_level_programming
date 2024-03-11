7b8b5c59c4021387a1eaf7e9441c13c4c612409c
Buno Woche <bunogache@gmail.com>
2024-03-11 20:54:24 +0300

    Test Some Things

---
+++ 0x0C-python-almost_a_circle/tests/test_somethings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" unittest module to show some tests"""
+
+
+import unittest
+
+
+class TestSomeThings(unittest.TestCase):
+    def test_upper(self):
+        self.assertEqual('foo'.upper, 'FOO')
+
+    def test_is_upper(self):
+        self.assertTrue('FOO'.isupper)
+        self.assertFalse('Foo'.isupper)
