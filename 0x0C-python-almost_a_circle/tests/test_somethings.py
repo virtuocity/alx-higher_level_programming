2a9dcf623c74895c3472747b8913b416a605b8de
Buno Woche <bunogache@gmail.com>
2024-03-11 20:59:06 +0300

    Test Some Things

--- 0x0C-python-almost_a_circle/tests/test_somethings.py
+++ 0x0C-python-almost_a_circle/tests/test_somethings.py
@@ -7,8 +7,12 @@ import unittest
 
 class TestSomeThings(unittest.TestCase):
     def test_upper(self):
-        self.assertEqual('foo'.upper, 'FOO')
+        self.assertEqual('foo'.upper(), 'FOO')
 
     def test_is_upper(self):
-        self.assertTrue('FOO'.isupper)
-        self.assertFalse('Foo'.isupper)
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+
+if __name__ == "__main__":
+    unittest.main()
