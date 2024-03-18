24d43809fca0a3e9b70cdcd49f849818758def0e
Buno Woche <bunogache@gmail.com>
2024-03-18 19:59:41 +0300

    modify == and != II

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,8 +6,8 @@ class MyInt(int):
     def __init__(self, value):
         self.value = value
 
-    def __eq__(self, num):
-        return self.value != num
+    def __eq__(self, numb):
+        return self.value != numb
 
-    def __ne__(self, num):
-        return self.value == num
+    def __ne__(self, numb):
+        return self.value == numb
