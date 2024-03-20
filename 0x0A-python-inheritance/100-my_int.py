491c27566cc97164e677527ddc8156deb961c3f7
gabaya <gabayaconsult@gmail.com>
2024-03-20 12:57:07 +0000

    modify clas intII

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,11 +3,32 @@
 
 
 class MyInt(int):
-    def __init__(self, value):
-        self.value = value
+	"""
+	MyInt - class to modify int
+	class from qhich it inherits
+	"""
+	def __init__(self, value):
+		"""
+		init - constructor
+		Args:
+			self- the object
+			value - int 
+		"""
+		  self.value = value
 
-    def __eq__(self, num):
-        return self.value != num
+	def __eq__(self, num):
+		"""eq - modify from == to !=
+		   Args:
+				self- instance of class
+				num-value to check
+		"""
+		   return self.value != num
 
-    def __ne__(self, numb):
-        return self.value == num
+	def __ne__(self, numb):
+		"""
+			ne - modify from != to ==
+			Args:
+			   self- instance of class
+			   num-value to check
+		"""
+		 return self.value == num
