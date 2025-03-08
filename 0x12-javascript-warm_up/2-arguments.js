f8b1c9370f098d471ab9b478b522a4810f2362e1
virtuocity_ubuntu <bunogache@gmail.com>
2025-03-08 12:24:42 +0300

    task III string output correction

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let numArgs;
 if (process.argv.length <= 2) {
-  numArgs = 'No arguments';
+  numArgs = 'No argument';
 } else if (process.argv.length === 3) {
   numArgs = 'Argument found';
 } else {
